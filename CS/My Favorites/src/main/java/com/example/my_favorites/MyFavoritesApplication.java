4c093d9414f93e2c3105453370b06e04da9ebf6c
cindyfeldman <cinderella.feldman@gmail.com>
2024-12-30 22:30:02 -0800

    First commit: set up thymeleaf and added necessary files

---
+++ CS/My Favorites/src/main/java/com/example/my_favorites/MyFavoritesApplication.java
@@ -0,0 +1,15 @@
+package com.example.my_favorites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+public class MyFavoritesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFavoritesApplication.class, args);
+
+    }
+
+}
---
+++ CS/My Favorites/src/test/java/com/example/my_favorites/MyFavoritesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.my_favorites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFavoritesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
